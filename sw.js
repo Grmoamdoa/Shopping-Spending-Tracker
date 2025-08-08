d852d8ccd9907c67017f54a641653be73bbaf49e
Grmoamdoa <greggeorge2002@icloud.com>
2025-08-07 20:01:09 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,28 @@
+// Minimal offline cache for GitHub Pages
+const CACHE = 'smartshop-v4-1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+  // External CDNs will be cached on first fetch automatically
+];
 
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (e)=>{
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res=>{
+      const copy = res.clone();
+      caches.open(CACHE).then(c=>c.put(req, copy)).catch(()=>{});
+      return res;
+    }).catch(()=> caches.match('/index.html')))
+  );
+});
